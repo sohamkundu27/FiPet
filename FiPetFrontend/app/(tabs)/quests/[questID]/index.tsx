93d507a8a55a446e13863053c1e19c1628713bc5
AndyK3 <jiwonk2023@gmail.com>
2025-07-13 15:06:58 -0500

    Made quests be user specific
    Get quest from firebase and display only the current quest on quest page. Also added passport for pet home.

--- FiPetFrontend/app/(tabs)/quests/[questID]/index.tsx
+++ FiPetFrontend/app/(tabs)/quests/[questID]/index.tsx
@@ -53,7 +53,11 @@ export default function Page() {
     return (<QuestComplete />);
   }
 
+  // If the quest has a preQuest requirement, redirect to preQuest reading first
+<<<<<<< HEAD
   // Check if there's a prereading that needs to be shown first
+=======
+>>>>>>> 321ddfa (Made quests be user specific)
   if (quest?.preQuest) {
     return (<Redirect href={`/quests/${questID}/preQuestReading`} />);
   }
