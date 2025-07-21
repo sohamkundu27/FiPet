5fb21a1401a4159b379fefc1e5c3299b05028540
Ryan Sauers <rlsauers08@gmail.com>
2025-07-21 16:21:45 -0500

    Schema change

--- FiPetFrontend/src/types/quest.ts
+++ FiPetFrontend/src/types/quest.ts
@@ -1,18 +1,18 @@
 import { Timestamp } from "@firebase/firestore";
 import { ItemId } from "./item";
 
-export const QUEST_COLLECTION = 'quests2';
-export const QUEST_COMPLETION_COLLECTION = 'questCompletion2';
-export const QUESTIONS_COLLECTION = 'questions2';
-export const ANSWER_COLLECTION = 'questAnswers2';
-export const OPTIONS_COLLECTION = 'options2';
-export const READING_COLLECTION = 'preQuestReadings2';
+export const QUEST_COLLECTION = 'quests';
+export const QUEST_COMPLETION_COLLECTION = 'questCompletion';
+export const QUESTIONS_COLLECTION = 'questions';
+export const ANSWER_COLLECTION = 'questAnswers';
+export const OPTIONS_COLLECTION = 'options';
+export const READING_COLLECTION = 'preQuestReadings';
 
 export type QuestionTypeWithSingleOption = "singleSelect";
 export type QuestionTypeWithMultipleOptions = never;
 export type QuestionTypeWithOptions = QuestionTypeWithSingleOption | QuestionTypeWithMultipleOptions;
 export type QuestionType = QuestionTypeWithOptions;
-export type QuestTopic = "Opportunity Cost" | "Budgeting";
+export type QuestTopic = "opportunity cost";
 
 export type QuestId = string;
 export type PreQuestReadingId = string;
@@ -92,10 +92,6 @@ export type DBOption<T extends QuestionType> = {
 } & DBOptionShape<T>;
 
 export type DBQuestCompletion = {
-<<<<<<< HEAD
-  id: QuestId,
-=======
->>>>>>> 0332845 (Schema change)
   questId: QuestId,
   completedAt: Timestamp,
   reward: Reward,
